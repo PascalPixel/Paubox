9c8ddd1cb034e8737ed89fcbf8a5b2d09f51baed
Pascal Pencil <pascal@superpencil.com>
2016-09-22 20:20:38 +0900

    HAML, homepage, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Root
+  root to: "welcomes#index"
+
+  # Reroute bad links to home
+  get "*path", to: "welcomes#index"
 end
