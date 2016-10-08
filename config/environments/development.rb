393a24a7d6ab6a60f970bdd9a29e3244a496c8c3
Pascal Pencil <pascal@superpencil.com>
2016-10-08 23:12:16 +0900

    About page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   config.assets.debug = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  config.action_view.raise_on_missing_translations = true
 
   # Default url for Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
